e44afd4326d813d52add40663075aa03098f6ae7
ReneI <reidisy@gmail.com>
2018-08-24 23:11:17 -0500

    yargs

--- node/bases-node/app2.js
+++ node/bases-node/app2.js
@@ -4,10 +4,14 @@ const { crearArchivo } = require('./multiplicar/mul.js');
 
 
 let argv2 = process.argv;
+
+console.log(argv);
+console.log(argv2);
+/* 
 let parametro = argv[2];
 let base = parametro.split('=')[1];
 console.log(base);
 
 crearArchivo(base)
     .then(archivo => console.log(`Archivos creado ${archivo}`))
-    .catch(e => { console.log(e) });
+    .catch(e => { console.log(e) }); */
