64899a9881449c5ce9f68fdb1ce527cf8b07f1f9
ReneI <reidisy@gmail.com>
2018-08-24 23:41:31 -0500

    con parametros

--- node/bases-node/app2.js
+++ node/bases-node/app2.js
@@ -1,12 +1,31 @@
 // requiereds 
-const argv = require('yargs').argv;
+const argv = require('yargs')
+    .command('listar', 'Imprime en consoa la tabla', {
+
+        base: {
+
+
+            demand: true,
+            alias: 'b'
+        },
+
+        limite: {
+
+            default: 10,
+            alias: 'l'
+
+        }
+
+
+    }).help()
+    .argv;
 const { crearArchivo } = require('./multiplicar/mul.js');
 
 
 let argv2 = process.argv;
 
 console.log(argv);
-console.log(argv2);
+console.log('limite', argv.limite);
 /* 
 let parametro = argv[2];
 let base = parametro.split('=')[1];
